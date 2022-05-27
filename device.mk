b98a3637256586da8aa68f6f60cdc3afe2721528
Arian <arian.kulmer@web.de>
2022-05-27 16:21:48 +0200

    cupid: Implement UDFPS
    Change-Id: Ib0a8ed7d6e794aef4ad02b960841c66dfad79390

--- device.mk
+++ device.mk
@@ -115,7 +115,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    libudfpshandler
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
