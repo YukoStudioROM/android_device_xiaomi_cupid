e28d962694e8ba3b9c42f084e75721cc10a6706d
Kujou Yuko <i@779.moe>
2023-01-24 13:11:46 +0800

    cupid: Build update engine
    Change-Id: I5546229fa71a4b26ea615e7b2380f297e4d78936

--- device.mk
+++ device.mk
@@ -63,3 +63,9 @@ PRODUCT_PACKAGES += \
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+
+# Update engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
