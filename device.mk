14023267230d2f5a9b2c2e12790cb6bebf3cc3f7
Kujou Yuko <i@779.moe>
2023-02-05 22:18:28 +0800

    cupid: Import missing WFD vendor blobs
    Change-Id: I73cef748bbe04cea6828308810a9dfa2d2a25918

--- device.mk
+++ device.mk
@@ -223,3 +223,7 @@ PRODUCT_COPY_FILES += \
 # WiFi
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml
+
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libwfdaac_vendor
