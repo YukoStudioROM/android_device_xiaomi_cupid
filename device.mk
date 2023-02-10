870eaf0636586ce9590ce881727c68057ae6ff2b
Kujou Yuko <i@779.moe>
2023-02-10 14:01:55 +0800

    cupid: Build common Xiaomi fingerprint HAL
    Change-Id: I6c6c59844102962cbb767a20eab345e99de80245

--- device.mk
+++ device.mk
@@ -114,6 +114,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
